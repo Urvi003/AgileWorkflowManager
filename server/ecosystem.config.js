60cf597eaa90a68a6a8e5ac2dbd656cb279d6123
Urvi_003 <urupatel6861@gmail.com>
2025-02-23 12:49:10 +0530

    Second Commit

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps:[
+        {
+            name:"AgileWorkfloManager",
+            script:"npm",
+            args:"run dev",
+            env:{
+                NODE_ENV:"development",
+            }
+        }
+    ]
+}
